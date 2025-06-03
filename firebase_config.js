793826f8859eaea775a40702e8a9b90612443f1b
rumiza-shaikh <ss4287@cornell.edu>
2025-06-02 23:08:06 -0400

    Create firebase_config.js

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAopqH9EWLJI1yCsOhqPgTH_dfsNL9K7l4",
+  authDomain: "inboxiq-659de.firebaseapp.com",
+  projectId: "inboxiq-659de",
+  storageBucket: "inboxiq-659de.firebasestorage.app",
+  messagingSenderId: "140316260198",
+  appId: "1:140316260198:web:30783f24fb1e2d6774baa7",
+  measurementId: "G-QNLFLVN45M"
+};
